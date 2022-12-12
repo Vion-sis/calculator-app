1a9381a25c24d156b552e30fbae72221d10f674a
Vion-sis <79034712+Vion-sis@users.noreply.github.com>
2022-12-12 21:11:34 +0900

    added js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
-function displayNumber(num) {
-  let textDisplay = document.querySelector("#input-text");
-  textDisplay.innerHTML = num;
-}
+let buttons = document.querySelectorAll("button");
+let equalBtn = document.getElementsByClassName("equalbtn");
+let clearBtn = document.querySelector("#clear");
+let deleteBtn = document.querySelector("#del");
+let DisplayScreen = document.querySelector("#input-text");
 
-let number = document.querySelector("#num");
-number.addEventListener("click", displayNumber);
+let realTimedisplayValue = [];
+
+clearBtn.addEventListener("click", () => {
+  realTimeScreenValue = [""];
+});
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (!btn.id.match("clear")) {
+      realTimedisplayValue.push(btn.value);
+      DisplayScreen.innerHTML = realTimedisplayValue.join("");
+
+      if (btn.classList.contains("num")) {
+        DisplayScreen.innerHTML = eval(realTimedisplayValue.join(""));
+      }
+    }
+    if (btn.id.match("erase")) {
+      realTimedisplayValue.pop();
+      DisplayScreen.innerHTML = realTimedisplayValue;
+    }
+    if (btn.id.match("evaluate")) {
+      realTimedisplayValue;
+    }
+
+    if (typeof eval(realTimeScreenValue.join("")) == "undefined") {
+      DisplayScreen.innerHTML = 0;
+    }
+  });
+});
